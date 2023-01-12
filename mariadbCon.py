d9bc7878238e0f5cf2d8efd70fa3c8c391dc9362
Saku <saku.kaarlejarvi@gmail.com>
2023-01-12 13:35:27 +0200

    Siistitty koodia

--- mariadbCon.py
+++ mariadbCon.py
@@ -49,45 +49,35 @@ alarms = []
 idletimes = []
 
 #File
-jsonBackupFile = '/home/pi/Desktop/sshVSC/jsonBackupMachine.json'
+jsonPath = '/home/pi/Desktop/sshVSC/'
+jsonBackupFile = f"{jsonPath}jsonBackupMachine1.json"
+print(jsonBackupFile)
 try:
-    jsonBackupFile = '/home/pi/Desktop/sshVSC/jsonBackupMachine.json'
-    with open(jsonBackupFile) as jsonData:
+    with open(jsonBackupFile,'a+') as jsonData:
         jsonData.seek(0)
         production_times = json.load(jsonData)
-
-        #print(production_times)
 except json.JSONDecodeError as e:
     print(e)
     pass
 
 class mainClass():
 
-    
-            
-
-    #return production_times,jsonBackupFile
-
-#production_times, jsonBackupFile = jsonBackup()
-
-
+#================================================================
+# Datan lähetys MariaDB -serveriin #
+#================================================================
     def dataSendDb(self,machine_id, start_time, end_time, duration,isFault):
         query = "INSERT INTO laserdata (machine_id, start_time, end_time, duration, isFault) " \
             "VALUES (%s,%s,%s,%s,%s)"
         args = (machine_id, start_time, end_time, duration, isFault)
         try:
 
-            #print(query, args)
-            #print(self.conn)
             print(self.conn.reconnect())
             print(self.conn.ping())
+
             self.cursor = self.conn.cursor()
-            #print("Cursor Closed:",self.cursor.closed)
-            #print("self.cursor = self.conn.cursor()",self.cursor)
             self.cursor.execute(query,args)
-            #print("self.cursor.execute(query,args)",self.cursor.execute(query,args))
             self.conn.commit()
-            #print("self.conn.commit()",self.conn.commit())
+
             time.sleep(0.1)
 
         except mariadb.Error as e:
@@ -101,12 +91,13 @@ class mainClass():
                 #production_times.append(data)
                 jsonData.seek(0)
                 json.dump(production_times,jsonData,indent=5)
-            #print("Cursor Closed:",self.cursor.closed)
+
             self.conn.commit()
             self.cursor.close()
             print("Cursor Closed:",self.cursor.closed)
-            
-            
+#================================================================
+# Kirjautumistiedoston luku ja kirjautuminen MariaDB serveriin #
+#================================================================
     def tryConnection(self):
 
         try:
@@ -115,21 +106,21 @@ class mainClass():
                 try:
                     print("Testing connection to Local MariaDB...")
                     
-                    # self.connParams = {
-                    #     "user":self.loginSettings["user"],
-                    #     "password":self.loginSettings["password"],
-                    #     "host":self.loginSettings["host"],
-                    #     "port":self.loginSettings["port"],
-                    #     "database":self.loginSettings["database"]}
-                    # self.conn = mariadb.connect(**self.connParams)
+                    self.connParams = {
+                        "user":self.loginSettings["user"],
+                        "password":self.loginSettings["password"],
+                        "host":self.loginSettings["host"],
+                        "port":self.loginSettings["port"],
+                        "database":self.loginSettings["database"]}
+                    self.conn = mariadb.connect(**self.connParams)
                     
                     #ALKUPERÄINEN CONNECTING
-                    self.conn = mariadb.connect(
-                        user=self.loginSettings["user"],
-                        password=self.loginSettings["password"],
-                        host=self.loginSettings["host"],
-                        port=self.loginSettings["port"],
-                        database=self.loginSettings["database"])
+                    # self.conn = mariadb.connect(
+                    #     user=self.loginSettings["user"],
+                    #     password=self.loginSettings["password"],
+                    #     host=self.loginSettings["host"],
+                    #     port=self.loginSettings["port"],
+                    #     database=self.loginSettings["database"])
                     print(self.conn)
                     self.conn.auto_reconnect = True
                     print(self.conn.auto_reconnect)
@@ -147,7 +138,9 @@ class mainClass():
 
         self.laserDataRead(machine_id, start_time,end_time,duration, isFault)
 
-    ### BACKUPSQL COMMAND ###
+#================================================================
+##### BACKUPSQL COMMAND #####
+#================================================================
     def backupSQL(self):
 
         dateTimeNowCall = datetime.now()
@@ -157,40 +150,30 @@ class mainClass():
         Popen([f"mysqldump -u SaKa -p{self.loginSettings['password']} esimDB > /home/pi/Desktop/SQLBU/testidbbackup{buDate}.sql"], stdout=PIPE,shell=True)
         time.sleep(0.1)
 
+#================================================================
+#Ping -komento
+#================================================================
     def servuPing(self):
 
         self.dateTimeNowString = datetime.now()
         self.dateTimePing =self.dateTimeNowString.strftime("%Y-%m-%d %H:%M:%S")
         #pinggaus = self.conn.reconnect()
         print(f"\nAutomatic ping to server...\nTime: {self.dateTimePing} \n")
-
-        ### Poistaa connParams ja muut, jättää vain self.cursor & conn.commit()
-
         try:
             self.conn.cursor()
-            # self.connParams = {
-            #     "user":self.loginSettings["user"],
-            #     "password":self.loginSettings["password"],
-            #     "host":self.loginSettings["host"],
-            #     "port":self.loginSettings["port"],
-            #     "database":self.loginSettings["database"]}
-
-            #self.conn# = mariadb.connect(**self.connParams)
-            #self.cursor# = self.conn.cursor()
-            #self.conn.commit()
-            #self.conn
             self.conn
             self.cursor
             self.conn.commit()
-
             
         except mariadb.Error as er:
             print('Unable to ping: ',er)
         finally:
             self.cursor.close()
         
-        
-        #print(self.conn.ping(True))
+#================================================================
+# Laser logiikka
+#================================================================
+
     def laserDataRead(self, machine_id, start_time,end_time,duration, isFault):
 
         global machine_state
@@ -198,7 +181,7 @@ class mainClass():
         
         #Määritetty backup tiedoston ajankohdat
         schedule.every().day.at("14:30").do(self.backupSQL).run
-        schedule.every().day.at("14:30").do(self.servuPing).run
+        #schedule.every().day.at("14:30").do(self.servuPing).run
         schedule.every(4).hours.do(self.servuPing).run
         #schedule.every(30).minutes.do(self.servuPing).run
         #schedule.every(1).minutes.do(self.servuPing).run
@@ -221,6 +204,7 @@ class mainClass():
                 elif power_on == True and alarm == False and laser == False and machine_state != MACHINE_STATE_IDLE :
                     print("Machine state: idle")
                     machine_state = MACHINE_STATE_IDLE
+
                 # machine state laser on, production running  
                 elif laser == True and power_on == True and alarm == False and  measuring_started == False and machine_state != MACHINE_STATE_RUNNING:
                     print("Laser ON")
@@ -245,14 +229,14 @@ class mainClass():
                         "Duration": str(duration),
                         "isFault" : str(isFault)
                         }
+
                     production_times.append(data)
                     measuring_started = False
-                    
+
                     print("\nMachine data:")
                     for datakey, datavalue in data.items():
                         print(datakey,":",datavalue)
                     time.sleep(0.1)
-
                     self.dataSendDb(machine_id, start_time, end_time, duration, isFault)                   
 
                 # machine state ALARM
@@ -275,38 +259,33 @@ class mainClass():
                         "Duration": str(duration),
                         "isFault" : str(isFault)
                     }
+
                     production_times.append(data)
+
                     print("\nMachine Data:")
                     for datakey, datavalue in data.items():
                         print(datakey,":",datavalue)
                     time.sleep(0.1)
-
                     self.dataSendDb(machine_id, start_time, end_time, duration, isFault)
 
                 schedule.run_pending()
                 time.sleep(0.3)
                 
-
-
         except KeyboardInterrupt:
             print("------------------")
             print("Production times:")
             #print(type(production_times))
             for DataItem in production_times:
                 print(DataItem)
-            
-            
             print(json.dumps(production_times,indent=5))
-
 
     def main(self):
         
         #Reading Login JSON
         #Trying to connect MariaDB using .JSON file
-        #self.jsonBackupRead(production_times)
         self.tryConnection()
     
 if __name__ == '__main__':
     paa = mainClass()
     paa.main()
-    #paa.kaikkiAsiakkaat()
+
