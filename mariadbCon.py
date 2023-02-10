18254c1091583fcbf1c6ccd04950ab887b400e5d
Saku <saku.kaarlejarvi@gmail.com>
2023-02-10 15:53:43 +0200

    update, fixattavaa

--- mariadbCon.py
+++ mariadbCon.py
@@ -15,28 +15,31 @@ import schedule
 
 # Signals from machine
 LASER_ON_SIGNAL = 23
-FAULT_SIGNAL = 24
+MACHINE_STANDBY = 24
 MACHINE_POWER_ON_SIGNAL= 25
 # GPIO setup
 # input connected to 3,3v
 # Pull down resistor mode activated to get solid 0 reading
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(LASER_ON_SIGNAL,GPIO.IN, pull_up_down =GPIO.PUD_DOWN)
-GPIO.setup(FAULT_SIGNAL,GPIO.IN, pull_up_down =GPIO.PUD_DOWN)
+GPIO.setup(MACHINE_STANDBY,GPIO.IN, pull_up_down =GPIO.PUD_DOWN)
 GPIO.setup(MACHINE_POWER_ON_SIGNAL,GPIO.IN, pull_up_down =GPIO.PUD_DOWN)
 
 # program started
 print("Program running...")
 # constants
 MACHINE_STATE_POWER_OFF = 0
-MACHINE_STATE_IDLE =1
-MACHINE_STATE_RUNNING = 2
-MACHINE_STATE_PART_READY = 3
-MACHINE_STATE_ALARM = 4
+MACHINE_STATE_IDLE = 1
+MACHINE_STATE_STANDBY = 2
+MACHINE_STATE_RUNNING = 3
+MACHINE_STATE_PART_READY = 4
+
 
 # variables
 machine_id ="Byfib8025"
+connection_succ = False
 measuring_started = False
+standby_measuring_started = False
 start_time = None
 end_time = None
 duration = None
@@ -70,7 +73,7 @@ class mainClass():
             "VALUES (%s,%s,%s,%s,%s)"
         args = (machine_id, start_time, end_time, duration, isFault)
         try:
-
+            
             print(self.conn.reconnect())
             print(self.conn.ping())
 
@@ -83,70 +86,143 @@ class mainClass():
         except mariadb.Error as e:
             print(f"Error connectiong to MariaDB Platform: {e}")
             time.sleep(0.1)
-            
-            
-            
+
         finally:
+            
+            self.conn.commit()
+            self.cursor.close()
+            print("Cursor Closed:",self.cursor.closed)
             with open(jsonBackupFile,'w') as jsonData:
                 #production_times.append(data)
                 jsonData.seek(0)
                 json.dump(production_times,jsonData,indent=5)
 
-            self.conn.commit()
-            self.cursor.close()
-            print("Cursor Closed:",self.cursor.closed)
-#================================================================
-# Kirjautumistiedoston luku ja kirjautuminen MariaDB serveriin #
-#================================================================
-    def tryConnection(self):
+            # self.conn.commit()
+            # self.cursor.close()
+            # print("Cursor Closed:",self.cursor.closed)
 
+##==================================================
+## Connect To MariaDB using JSON -file
+##==================================================
+##
+## Koodi virheellinen, keksittävä "Jos Virhe, Ota yhteys lokaaliin" jne...
+##
+    def ConnectMariaDBJSON(self):
         try:
             with open('/home/pi/Desktop/sshVSC/userconf24.json','r') as loginData:
                 self.loginSettings = json.load(loginData)
                 try:
-                    print("Testing connection to Local MariaDB...")
-                    
+                    print("Testing connection to Server MariaDB...")
+
                     self.connParams = {
                         "user":self.loginSettings["user"],
                         "password":self.loginSettings["password"],
-                        "host":self.loginSettings["host"],
+                        "host":"localhost",
                         "port":self.loginSettings["port"],
                         "database":self.loginSettings["database"]}
+
                     self.conn = mariadb.connect(**self.connParams)
-                    
+                    self.conn.auto_reconnect = True
+                    print(self.conn.auto_reconnect)
+                    self.cursor = self.conn.cursor()
+                    self.cursor.close()
+                    print("Cursor closed...",self.cursor.closed)
+
                     #ALKUPERÄINEN CONNECTING
                     # self.conn = mariadb.connect(
                     #     user=self.loginSettings["user"],
                     #     password=self.loginSettings["password"],
                     #     host=self.loginSettings["host"],
                     #     port=self.loginSettings["port"],
-                    #     database=self.loginSettings["database"])
+                    # print(self.conn)
+
+
+
+                except Exception as e:
+                    print(f"Error occurred... '{e}' Couldn't connect to MariaDB server")#Check connection.\nPing: ",self.conn.ping
+                    #self.connParams == None
+                    #raise ConnectionError
+                finally:
+                    #self.conn.close()
+                    return connection_succ == False
+
+
+        except IOError as ose:
+            print("Virhe, ei pystytä lukemaan tiedostoa...")
+            raise FileNotFoundError(f"{ose}, can't read file: \n")
+
+    def ConnectLocalMariaDB(self):
+        try:
+            with open('/home/pi/Desktop/sshVSC/userconf24.json','r') as loginData:
+                self.loginSettings = json.load(loginData)
+                try:
+                    print("Testing connection to Localhost MariaDB...")
+                    self.connParams = {
+                        "user":self.loginSettings["user"],
+                        "password":self.loginSettings["password"],
+                        "host":"localhost",
+                        "port":self.loginSettings["port"],
+                        "database":self.loginSettings["database"]}
+                    self.conn = mariadb.connect(**self.connParams)
+
+                            #ALKUPERÄINEN CONNECTING
+                            # self.conn = mariadb.connect(
+                            #     user=self.loginSettings["user"],
+                            #     password=self.loginSettings["password"],
+                            #     host=self.loginSettings["host"],
+                            #     port=self.loginSettings["port"],
+                            #     database=self.loginSettings["database"])
+                    self.conn = mariadb.connect(**self.connParams)
                     print(self.conn)
                     self.conn.auto_reconnect = True
                     print(self.conn.auto_reconnect)
                     self.cursor = self.conn.cursor()
                     self.cursor.close()
                     print("Cursor closed...",self.cursor.closed)
+
+                except Exception as e:
+                    print(f"Error occurred... '{e}' Couldn't connect to MariaDB server")#Check connection.\nPing: ",self.conn.ping
+                    #pass
+                    #self.conn.close()
+                    #raise ConnectionError
                     
-                    
-                except mariadb.Error as e:
-                    raise ConnectionError(f"Error occurred... '{e}' Couldn't connect to MariaDB server. Check connection.\nPing: ",self.conn.ping)
+                finally:
+                    # print(self.conn)
+                    # self.conn.auto_reconnect = True
+                    # print(self.conn.auto_reconnect)
+                    # self.cursor = self.conn.cursor()
+                    # self.cursor.close()
+                    # print("Cursor closed...",self.cursor.closed)
+                    return connection_succ == True
 
         except IOError as ose:
             print("Virhe, ei pystytä lukemaan tiedostoa...")
             raise FileNotFoundError(f"{ose}, can't read file: \n")
 
-        self.laserDataRead(machine_id, start_time,end_time,duration, isFault)
+#================================================================
+# Kirjautumistiedoston luku ja kirjautuminen MariaDB serveriin #
+#================================================================
 
+    def tryConnection(self):
+        #if ConnectionSucc == True:
+            #self.ConnectMariaDBJSON()
+            #self.laserDataRead(machine_id, start_time,end_time,duration, isFault)
+        #el
+        if connection_succ == False:
+            self.ConnectLocalMariaDB()
+            self.laserDataRead(machine_id, start_time,end_time,duration, isFault)
+        else:
+            print("error:",mariadb.Error)
+            sys.exit()
 #================================================================
 ##### BACKUPSQL COMMAND #####
 #================================================================
     def backupSQL(self):
 
         dateTimeNowCall = datetime.now()
-        print("SQL Backup Created:", dateTimeNowCall) 
+        print("SQL Backup Created:", dateTimeNowCall)
         buDate = dateTimeNowCall.strftime("%Y-%m-%d_%H%M%S")
-        #Backup komento. 
+        #Backup komento.
         Popen([f"mysqldump -u SaKa -p{self.loginSettings['password']} esimDB > /home/pi/Desktop/SQLBU/testidbbackup{buDate}.sql"], stdout=PIPE,shell=True)
         time.sleep(0.1)
 
@@ -164,12 +240,12 @@ class mainClass():
             self.conn
             self.cursor
             self.conn.commit()
-            
+
         except mariadb.Error as er:
             print('Unable to ping: ',er)
         finally:
             self.cursor.close()
-        
+
 #================================================================
 # Laser logiikka
 #================================================================
@@ -178,50 +254,96 @@ class mainClass():
 
         global machine_state
         global measuring_started
-        
+        global standby_measuring_started
+
         #Määritetty backup tiedoston ajankohdat
         schedule.every().day.at("12:00").do(self.backupSQL).run
         schedule.every().day.at("12:00").do(self.servuPing).run
         schedule.every().sunday.at("12:00").do(self.backupSQL).run
         schedule.every(4).hours.do(self.servuPing).run
-        #schedule.every(30).minutes.do(self.servuPing).run
+        schedule.every(30).minutes.do(self.servuPing).run
+        schedule.every(30).minutes.do(self.backupSQL).run
         #schedule.every(1).minutes.do(self.servuPing).run
-        
 
+        ## Lisättävä 2 elif osaa? Jossa sammutetaan 
         try:
             while True:
 
                 laser = GPIO.input(LASER_ON_SIGNAL)
-                alarm = GPIO.input(FAULT_SIGNAL)
+                standby = GPIO.input(MACHINE_STANDBY)
                 power_on = GPIO.input(MACHINE_POWER_ON_SIGNAL)
 
-                # machine state OFF  
-                if power_on == False and alarm == False and laser == False and machine_state != MACHINE_STATE_POWER_OFF:
+                # machine state OFF
+                if power_on == False and standby == False and laser == False and machine_state != MACHINE_STATE_POWER_OFF:
                     machine_state = MACHINE_STATE_POWER_OFF
                     print("Machine state: Power OFF")
                     #Power OFF timer?
 
-                # machine state IDLE    
-                elif power_on == True and alarm == False and laser == False and machine_state != MACHINE_STATE_IDLE :
+                # machine state IDLE
+                elif power_on == True and standby == False and laser == False and machine_state != MACHINE_STATE_IDLE:
                     print("Machine state: idle")
                     machine_state = MACHINE_STATE_IDLE
+                
+                # elif machine_state == MACHINE_STATE_STANDBY:
+                #     kakka = "💩"
+                #     print("sheeeesh 😎")
+                #     machine_state = 0
+                #     if kakka == kakka:
+                #         print("😲")
 
-                # machine state laser on, production running  
-                elif laser == True and power_on == True and alarm == False and  measuring_started == False and machine_state != MACHINE_STATE_RUNNING:
+                # machine standby and waiting for commmand
+                elif power_on == True and standby == True and laser == False and standby_measuring_started == False and machine_state != MACHINE_STATE_STANDBY:
+                    print("Machine is standby and waiting...")
+                    machine_state = MACHINE_STATE_STANDBY
+                    print("Testi, katsotaan laskeeko aikaa...")
+                    start_time = datetime.now()
+                    isFault = True
+                    standby_measuring_started = True
+                    measuring_started == False
+
+                # machine state laser on, production running
+                elif power_on == True and standby == True and laser == True and measuring_started == False and machine_state != MACHINE_STATE_RUNNING:
                     print("Laser ON")
-                    print("Machine state: Running")
+                    print("Machine state: Running\n")
+                    
+
+                    end_time = datetime.now()
+                    duration = end_time - start_time
+                    print("Standby time :",duration)
+                    data = {
+                        "Machine ID":machine_id,
+                        "Start":str(start_time) ,
+                        "End": str(end_time),
+                        "Duration": str(duration),
+                        "isFault" : str(isFault)
+                        }
+
+                    production_times.append(data)
+
+                    print("\nMachine data:")
+                    for datakey, datavalue in data.items():
+                        print(datakey,":",datavalue)
+
+                    self.dataSendDb(machine_id, start_time, end_time, duration, isFault)
+
+                    standby_measuring_started = False
                     machine_state = MACHINE_STATE_RUNNING
                     start_time = datetime.now()
                     measuring_started = True
-                # machine state production end  
-                elif laser == False and measuring_started == True and machine_state !=MACHINE_STATE_PART_READY:
+                    time.sleep(0.1)
+
+                # machine state production end
+                elif power_on == True and standby == True and laser == False and measuring_started == True and machine_state !=MACHINE_STATE_PART_READY:
                     print("Laser OFF")
                     print("Machine state: Part ready")
-                    machine_state = MACHINE_STATE_PART_READY
+                    measuring_started = False
+                    machine_state -= 1 #MACHINE_STATE_PART_READY
                     end_time = datetime.now()
                     duration = end_time- start_time
-                    print("start time: ", start_time)
-                    print("duration: ", duration)
+
+                    #print("start time: ", start_time)
+                    #print("duration: ", duration)
+
                     isFault = False
                     data = {
                         "Machine ID":machine_id,
@@ -232,46 +354,49 @@ class mainClass():
                         }
 
                     production_times.append(data)
-                    measuring_started = False
+                    self.dataSendDb(machine_id, start_time, end_time, duration, isFault)
 
+                    standby_measuring_started = False
                     print("\nMachine data:")
                     for datakey, datavalue in data.items():
                         print(datakey,":",datavalue)
+
                     time.sleep(0.1)
-                    self.dataSendDb(machine_id, start_time, end_time, duration, isFault)                   
+                    
 
-                # machine state ALARM
-                elif laser == True and measuring_started == True  and alarm == True and machine_state != MACHINE_STATE_ALARM  :
-                    print("Machine state: Alarm")
-                    machine_state = MACHINE_STATE_ALARM  
-                    end_time = datetime.now()
-                    duration = end_time- start_time
-                    print("Fault detected", end_time)
-                    print("Cutting interrupted! timestamp: ", end_time)
-                    print("last laser on time:", start_time)
-                    print("Duration: ",duration)
-                    measuring_started = False
-                    isFault = True
 
-                    data = {
-                        "Machine ID":machine_id,
-                        "Start":str(start_time) ,
-                        "End": str(end_time),
-                        "Duration": str(duration),
-                        "isFault" : str(isFault)
-                    }
+                # # machine state ALARM
+                # elif laser == True and measuring_started == True  and standby == True and machine_state != MACHINE_STATE_STANDBY  :
+                #     print("Machine state: Alarm")
+                #     machine_state = MACHINE_STATE_STANDBY
+                #     end_time = datetime.now()
+                #     duration = end_time- start_time
+                #     print("Fault detected", end_time)
+                #     print("Cutting interrupted! timestamp: ", end_time)
+                #     print("last laser on time:", start_time)
+                #     print("Duration: ",duration)
+                #     measuring_started = False
+                #     isFault = True
 
-                    production_times.append(data)
+                #     data = {
+                #         "Machine ID":machine_id,
+                #         "Start":str(start_time),
+                #         "End": str(end_time),
+                #         "Duration": str(duration),
+                #         "isFault" : str(isFault)
+                #     }
 
-                    print("\nMachine Data:")
-                    for datakey, datavalue in data.items():
-                        print(datakey,":",datavalue)
-                    time.sleep(0.1)
-                    self.dataSendDb(machine_id, start_time, end_time, duration, isFault)
+                #     production_times.append(data)
+
+                #     print("\nMachine Data:")
+                #     for datakey, datavalue in data.items():
+                #         print(datakey,":",datavalue)
+                #     time.sleep(0.1)
+                #     self.dataSendDb(machine_id, start_time, end_time, duration, isFault)
 
                 schedule.run_pending()
                 time.sleep(0.3)
-                
+
         except KeyboardInterrupt:
             print("------------------")
             print("Production times:")
@@ -281,11 +406,11 @@ class mainClass():
             print(json.dumps(production_times,indent=5))
 
     def main(self):
-        
+
         #Reading Login JSON
         #Trying to connect MariaDB using .JSON file
         self.tryConnection()
-    
+
 if __name__ == '__main__':
     paa = mainClass()
     paa.main()
